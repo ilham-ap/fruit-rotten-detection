6051cc27092c3ef9a10bcbe6f404411e50648e12
Ilham Adi Panuntun <36600653+ilham-ap@users.noreply.github.com>
2024-01-20 08:04:49 +0800

    rotten

---
+++ precache-manifest.57f78d8d4caf22072566ef3ce89379d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fa4b15523c0a2200ad2ce9f9a0f90d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0662ac4acae6c57fea88",
+    "url": "/static/css/2.d66afd4b.chunk.css"
+  },
+  {
+    "revision": "ceb92d195ca5ca990933",
+    "url": "/static/css/main.99e7484e.chunk.css"
+  },
+  {
+    "revision": "0662ac4acae6c57fea88",
+    "url": "/static/js/2.875b8c2c.chunk.js"
+  },
+  {
+    "revision": "ceb92d195ca5ca990933",
+    "url": "/static/js/main.b20b410f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
